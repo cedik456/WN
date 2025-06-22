5ea5aa4080e47480c6c6173344ed38328667354c
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-06-22 23:39:15 +0800

    .

--- frontend/app/(auth)/login.jsx
+++ frontend/app/(auth)/login.jsx
@@ -1,23 +1,18 @@
-import { useNavigation } from "expo-router";
-import { useRouter } from "expo-router";
-import {
-  Text,
-  View,
-  SafeAreaView,
-  Pressable,
-} from "react-native";
+import { useRouter } from 'expo-router';
+import { Text, View, SafeAreaView, Pressable } from 'react-native';
 
 const Login = () => {
   const router = useRouter();
   return (
     <View className="flex-1 bg-black">
       <SafeAreaView className="justify-between flex-1 px-6 py-10 bg-black">
-        
         {/* Logo */}
         <View className="items-center flex-1 space-y-2 mt-60 ">
           {/* Change to image to change the logo */}
           <Text className="text-4xl font-bold text-white">Who's Next?</Text>
-          <Text className="text-sm font-medium text-white">Swipe, Match, Hire</Text>
+          <Text className="text-sm font-medium text-white">
+            Swipe, Match, Hire
+          </Text>
         </View>
 
         {/* Info about creating account */}
@@ -29,23 +24,31 @@ const Login = () => {
 
         {/* Action Buttons */}
         <View className="flex-1">
-          <Pressable className="items-center p-4 m-2 bg-white rounded-full"
-          >
+          <Pressable className="items-center p-4 m-2 bg-white rounded-full">
             <Text className="font-medium text-black">Continue with Google</Text>
           </Pressable>
 
           <Pressable className="items-center p-4 m-2 bg-white rounded-full">
-            <Text className="font-medium text-black">Create account with Email</Text>
+            <Text className="font-medium text-black">
+              Create account with Email
+            </Text>
           </Pressable>
 
-          <Pressable className="items-center p-4 m-2 bg-white rounded-full" onPress={() => router.push('/(auth)/signup')}>
-            <Text className="font-medium text-black">Use cell phone number</Text>
+          <Pressable
+            className="items-center p-4 m-2 bg-white rounded-full"
+            onPress={() => router.push('/(auth)/signup')}
+          >
+            <Text className="font-medium text-black">
+              Use cell phone number
+            </Text>
           </Pressable>
         </View>
 
         {/* Footer */}
         <View className="items-center mt-6 mb-10">
-          <Text className="text-sm text-white underline">Trouble signing in?</Text>
+          <Text className="text-sm text-white underline">
+            Trouble signing in?
+          </Text>
         </View>
       </SafeAreaView>
     </View>
