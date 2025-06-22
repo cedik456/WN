5fc13c050b350896ac68b229232ab65fd3ec8bb4
Louis Jasper A. Cañon <132154593+Capsaicin-Enthusiast@users.noreply.github.com>
2025-06-23 00:06:39 +0800

    .

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,77 +1,76 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
-import Swiper from "react-native-deck-swiper";
-import ProfileCard from "../../components/ProfileCard";
-import Avatar1 from "../../assets/Avatars/Avatar1.png";
-import Avatar2 from "../../assets/Avatars/Avatar2.png";
-import Avatar3 from "../../assets/Avatars/Avatar3.png";
-import Avatar5 from "../../assets/Avatars/Avatar5.png";
-import Avatar6 from "../../assets/Avatars/Avatar6.png";
-import { useRouter } from "expo-router";
-import HomepageLogo from "../../assets/Logo3.png";
+import { Image, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
+import Swiper from 'react-native-deck-swiper';
+import ProfileCard from '../../components/ProfileCard';
+import Avatar1 from '../../assets/Avatars/Avatar1.png';
+import Avatar2 from '../../assets/Avatars/Avatar2.png';
+import Avatar3 from '../../assets/Avatars/Avatar3.png';
+import Avatar5 from '../../assets/Avatars/Avatar5.png';
+import Avatar6 from '../../assets/Avatars/Avatar6.png';
+import { useRouter } from 'expo-router';
+import HomepageLogo from '../../assets/Logo3.png';
 
 const Home = () => {
   const router = useRouter();
   const mockProfiles = [
     {
-      name: "Charles, 24",
-      job: "Software Engineer",
-      experience: "3 years of experience",
-      skills: ["React", "MongoDb", "JavaScript", "Tailwind", "Github", "PHP"],
-      bgColor: "#fce7f3",
+      name: 'Charles, 24',
+      job: 'Software Engineer',
+      experience: '3 years of experience',
+      skills: ['React', 'MongoDb', 'JavaScript', 'Tailwind', 'Github', 'PHP'],
+      bgColor: '#fce7f3',
       avatar: Avatar6,
     },
     {
-      name: "Isabelle, 23",
-      job: "UI/UX Designer",
-      experience: "Intern",
-      skills: ["Figma", "Sketch", "Adobe XD"],
-      bgColor: "#ede9fe",
+      name: 'Isabelle, 23',
+      job: 'UI/UX Designer',
+      experience: 'Intern',
+      skills: ['Figma', 'Sketch', 'Adobe XD'],
+      bgColor: '#ede9fe',
       avatar: Avatar2,
     },
     {
-      name: "Louis, 23",
-      job: "Full Stack Developer",
-      experience: "2 years of experience",
-      skills: ["React", "MongoDb", "Tailwind", "Github"],
-      bgColor: "#fbfbfb",
+      name: 'Louis, 23',
+      job: 'Full Stack Developer',
+      experience: '2 years of experience',
+      skills: ['React', 'MongoDb', 'Tailwind', 'Github'],
+      bgColor: '#fbfbfb',
       avatar: Avatar3,
     },
     {
-      name: "Mark, 28",
-      job: "UI/UX Designer and Developer",
-      experience: "1 year of experience",
-      skills: ["Figma", "Sketch", "Illustrator", "Adobe XD"],
-      bgColor: "#fef9c3",
+      name: 'Mark, 28',
+      job: 'UI/UX Designer and Developer',
+      experience: '1 year of experience',
+      skills: ['Figma', 'Sketch', 'Illustrator', 'Adobe XD'],
+      bgColor: '#fef9c3',
       avatar: Avatar5,
     },
     {
-      name: "Carl, 28",
-      job: "Frontend Developer",
-      experience: "3 years of experience",
-      skills: ["Figma", "JavaScript", "React", "Tailwind"],
-      bgColor: "#fbfbfb",
+      name: 'Carl, 28',
+      job: 'Frontend Developer',
+      experience: '3 years of experience',
+      skills: ['Figma', 'JavaScript', 'React', 'Tailwind'],
+      bgColor: '#fbfbfb',
       avatar: Avatar1,
     },
   ];
 
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={["left", "right", "bottom"]}>
-      <View style={styles.container}>
+    <SafeAreaView className="flex-1" edges={['left', 'right', 'bottom']}>
+      <View className="px-5 bg-white">
         {/* Header */}
-        <View style={styles.header}>
+        <View className="flex-row items-center justify-between my-5">
           <Image source={HomepageLogo} />
           <Ionicons
             name="options-outline"
             size={24}
             color="#000"
-            onPress={() => router.push("./filters")}
+            onPress={() => router.push('./filters')}
           />
         </View>
       </View>
-      <View style={styles.swipeContainer}>
+      <View className="flex-1 items-center justify-center">
         <Swiper
           cards={mockProfiles}
           renderCard={(card, index) => <ProfileCard key={index} card={card} />}
@@ -135,30 +134,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  swipeContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  flexRow: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  container: {
-    paddingHorizontal: 20,
-    backgroundColor: "white",
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginVertical: 20,
-  },
-  avatar: {
-    alignSelf: "center",
-    marginTop: 50,
-    marginBottom: 20,
-  },
-});
